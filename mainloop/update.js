6bd0d69965499f566329f56a8e210829279662a7
Hangikjet <kallibach@gmail.com>
2015-03-31 01:38:53 +0000

    'P' toggles pause

--- mainloop/update.js
+++ mainloop/update.js
@@ -1,6 +1,14 @@
 var NOMINAL_UPDATE_INTERVAL = 16.666;
+var paused = false;
 
 var update = function(dt) {
+	if (eatKey(80)) { // P button
+        paused = !paused;
+    }
+
+    if (paused)
+    	return;
+
     var original_dt = dt;
     var du = (dt / NOMINAL_UPDATE_INTERVAL);
     updateSimulation(du);
