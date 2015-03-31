5935e034df906c34e8199eeb922f9c69e72cb885
Hangikjet <kallibach@gmail.com>
2015-03-31 01:40:04 +0000

    remove tab

--- mainloop/update.js
+++ mainloop/update.js
@@ -2,12 +2,12 @@ var NOMINAL_UPDATE_INTERVAL = 16.666;
 var paused = false;
 
 var update = function(dt) {
-	if (eatKey(80)) { // P button
+    if (eatKey(80)) { // P button
         paused = !paused;
     }
 
     if (paused)
-    	return;
+        return;
 
     var original_dt = dt;
     var du = (dt / NOMINAL_UPDATE_INTERVAL);
